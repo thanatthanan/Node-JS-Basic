18fd61eda322eee22c8336bcb8deb716468fa791
thanatthanan <125787441+thanatthanan@users.noreply.github.com>
2023-02-19 13:49:55 +0700

    Add all files
    frist node js

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+// const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const app = express();
+const port = 3000;
+
+app.use(morgan('combined'));
+
+app.get("/",(req,res) =>{
+    res.send('my name is maybay');
+})
+
+app.listen(port,()=>{
+    // console.log('Listening on port : '+ port);
+    // debug('Listening on port : ' + port);
+    console.log('Listening on port : '+ port);
+})
